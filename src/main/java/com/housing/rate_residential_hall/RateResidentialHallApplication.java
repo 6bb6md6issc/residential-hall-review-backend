8022345f00decd124632823ce60c2f015ee52a26
6bb6md6issc <kevin9966hu@gmail.com>
2025-05-22 12:35:07 -0500

    set up project

---
+++ src/main/java/com/housing/rate_residential_hall/RateResidentialHallApplication.java
@@ -0,0 +1,13 @@
+package com.housing.rate_residential_hall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateResidentialHallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateResidentialHallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/housing/rate_residential_hall/RateResidentialHallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housing.rate_residential_hall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateResidentialHallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
